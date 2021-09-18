8424fda354ca8a34516e129eab7cdcaa44218f95
idiatou <idiatoudiallo1741@gmail.com>
2021-09-18 13:16:48 -0400

    index.css and app.js update

---
+++ react-task/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="container">
+  <Header/>
+    </div>
+  );
+}
+
+
+
+export default App;
